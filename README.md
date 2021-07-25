6d88142e8645f207a39032b7e53adde792517e1e
Mohammed-Alramahi <vittosc1997@gmail.com>
2021-07-26 00:17:40 +0300

    lab 16

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # aws-server
+
+MY deployed Link:
+
+http://awsserver-env-1.eba-4mgcbtxf.us-east-1.elasticbeanstalk.com/
---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const port = process.env.PORT || 5000;
+const app = express();
+app.use('/', express.static('./public'));
+app.get('/data', (req, res) => {
+    res.json({ test: 1, test2: 2 });
+})
+app.listen(port, () => console.log("listening"));
