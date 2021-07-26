fc492e9a167b31825c68a92d792906949a18884c
Mohammed-Alramahi <vittosc1997@gmail.com>
2021-07-26 10:10:03 +0300

    fixes

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 MY deployed Link:
 
-http://awsserver-env-1.eba-4mgcbtxf.us-east-1.elasticbeanstalk.com/
+http://myawsserver-env.eba-zmamenss.us-east-1.elasticbeanstalk.com/
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const port = process.env.PORT || 5000;
 const app = express();
-app.use('/', express.static('./public'));
+app.use(express.static('./public'));
 app.get('/data', (req, res) => {
     res.json({ test: 1, test2: 2 });
 })
